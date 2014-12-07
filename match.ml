79a0139c6ccf3cab31456e853fa8be1309e2e092
Oscar Moll <rimoll@gmail.com>
2014-12-06 21:19:37 -0800

    add basic regex parser

--- match.ml
+++ match.ml
@@ -1,23 +1,90 @@
-
-(** 
-grammar A. sexp like. 
-pattern -> "Lit" string
-pattern -> "Cat" (pattern, pattern)
-pattern -> "Or" (pattern, pattern)
-pattern -> "Star" pattern
-
-patern:
-| char
-| pattern *
-| pattern | pattern
-| pattern pattern
-| (pattern)
- *)
-
-(*let parsepat str = *)
-
 type pattern = Cat of pattern * pattern | Star of pattern |
-	       Or of pattern * pattern | Lit of string with sexp
+	       Or of pattern * pattern | Lit of string with sexp;;
+
+type parsecase = { pat:string; expected:pattern } with sexp;;
+
+let parsecases =  [
+    { pat="a"; expected=Lit "a" };
+    { pat="ab"; expected=Cat (Lit "a", Lit "b") };
+    { pat="abc"; expected=Cat (Lit"a", Cat (Lit "b", Lit "c")) };
+    { pat="a|b"; expected=Or (Lit "a", Lit "b") };
+    { pat="a|b|c"; expected=Or (Lit "a", Or (Lit "b", Lit "c")) };
+    { pat="a*"; expected=Star (Lit "a") };
+    { pat="ab*"; expected=Cat (Lit "a", Star (Lit "b")) };
+    { pat="a|bc"; expected=Or (Lit "a", Cat (Lit "b", Lit "c")) };
+    { pat="(ab)*"; expected=Star ( Cat (Lit "a", Lit "b") ) }
+  ];;
+
+
+(*
+root -> allpat []
+allpat -> starcat 
+allpat -> starcat | allpat 
+starcat -> starp
+starcat -> starp starcat 
+starp -> simplep
+starp -> simplep *
+simplep -> simple char
+simplep -> (allpat)
+
+after(allpat) =  [], )
+after(starcat): after(allpat) U  |
+after(starp):  after(starcat) U  begin(starcat)
+after(simplep): after(startp) U *
+*)
+
+let parsepat (str : string) : pattern = 
+  let debug_str (name: string) (str: char list) = 
+    Printf.printf "Debug at %s: %s\n%!"  name 
+		   (Sexp.to_string  (sexp_of_list sexp_of_char str)) in    
+  let err_str (site :string) (str : char list)  =
+    Printf.sprintf 
+      "(context %s) unexpected rem=%s" 
+      site (Sexp.to_string  (sexp_of_list sexp_of_char str)) in
+  let rec allpat (str : char list) : pattern * char list = 
+    let (pat, rem) = starcat str in 
+    match rem with 
+    | [] | ')' :: _ -> (pat, rem)
+    | '|' :: rem2 -> let (secondpat, rem3) = allpat rem2 in 
+		  (Or(pat, secondpat), rem3)
+    | x -> failwith (err_str "1" rem)
+  and starcat (str : char list) : pattern * char list = 
+    let (pat, rem) = starp str in 
+    match rem with 
+    | [] | (')' | '|'):: _  -> (pat,rem)
+    | _ -> let (secondpat, rem2) = starcat rem in 
+	   (Cat(pat, secondpat), rem2)
+  and starp (str: char list) : pattern * char list  =
+    let (pat, rem) = simplep str in 
+    match rem with 
+    | '*':: rem2 -> (Star(pat), rem2)
+    | _ -> (pat, rem)
+  and simplep (str: char list) : pattern * char list = 
+    match str with 
+    | '(' :: rem -> let (pat, rem2) = allpat rem in
+		    (match rem2 with 
+		     | ')':: rem3 -> (pat, rem3)
+		     | _ -> failwith (err_str "2" rem) )
+    | x :: rem -> (Lit (Char.to_string x), rem)
+    | [] -> failwith "expected more input"
+  in let (pat, rem) = allpat (String.to_list  str) in 
+     match rem with
+	 | [] -> pat
+	 | _ -> failwith (err_str "3" rem)
+
+
+						    
+let runparsetest (case : parsecase)  =
+  if (try parsepat case.pat with
+      | e -> failwith (Printf.sprintf
+			 "Exception %s thrown. Failed parse testcase: %s\n%!"
+			 (Sexp.to_string (sexp_of_exn e)) (Sexp.to_string (sexp_of_parsecase case))))
+     = case.expected then () else
+    failwith (Printf.sprintf
+		"Failed parse testcase: %s\n%!"
+		(Sexp.to_string (sexp_of_parsecase case)))
+
+let () = List.iter parsecases ~f:runparsetest
 
 let patmatch pattern (str : string) = 
   let rec pathelp pattern (chars: char list) =  match pattern with 
@@ -75,3 +142,7 @@ let runTest case =
 		(Sexp.to_string (sexp_of_testcase case)))
 
 let () = List.iter testcases ~f:runTest
+
+
+let matches (pat:string) (str:string) =
+  patmatch (parsepat pat) str
